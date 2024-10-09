0d4254dd8d9e2e8225d2c83d576b7877f578dcd0
Nihat Bayram <nbcakar@gmail.com>
2024-10-09 18:05:31 +0300

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,192 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import moment from 'moment';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import Calendar from './Calendar';
+import CalendarControls from './CalendarControls';
+import './style.css';
+
+
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    const today = moment();
+
+    const customCSSclasses = {
+      holidays: ['2024-04-25', '2024-05-01', '2024-06-02', '2024-08-15', '2024-11-01'],
+      spring: {
+        start: '2024-03-21',
+        end: '2024-6-20'
+      },
+      summer: {
+        start: '2024-06-21',
+        end: '2024-09-22'
+      },
+      autumn: {
+        start: '2024-09-23',
+        end: '2024-12-21'
+      },
+      weekend: 'Sat,Sun',
+      winter: day => day.isBefore(moment([2024, 2, 21])) || day.isAfter(moment([2024, 11, 21]))
+    };
+    // alternatively, customClasses can be a function accepting a moment object. For example:
+    // day => (day.isBefore(moment([day.year(),2,21])) || day.isAfter(moment([day.year(),11,21]))) ? 'winter': 'summer'
+
+    this.state = {
+      year: today.year(),
+      selectedDay: today,
+      selectedRange: [today, moment(today).add(7, 'day')],
+      showDaysOfWeek: true,
+      showTodayBtn: true,
+      showWeekSeparators: true,
+      selectRange: false,
+      firstDayOfWeek: 0, // sunday
+      customCSSclasses
+    };
+  }
+
+  onPrevYear() {
+    this.setState(prevState => ({
+      year: prevState.year - 1
+    }));
+  }
+
+  onNextYear() {
+    this.setState(prevState => ({
+      year: prevState.year + 1
+    }));
+  }
+
+  goToToday() {
+    const today = moment();
+
+    this.setState({
+      selectedDay: today,
+      selectedRange: [today, moment(today).add(7, 'day')],
+      year: today.year()
+    });
+  }
+
+  datePicked(date) {
+    this.setState({
+      selectedDay: date,
+      selectedRange: [date, moment(date).add(7, 'day')]
+    });
+  }
+
+  rangePicked(start, end) {
+    this.setState({
+      selectedRange: [start, end],
+      selectedDay: start
+    });
+  }
+
+  toggleShowDaysOfWeek() {
+    this.setState(prevState => ({
+      showDaysOfWeek: !prevState.showDaysOfWeek
+    }));
+  }
+
+  toggleForceFullWeeks() {
+    this.setState(prevState => ({
+      showDaysOfWeek: true,
+      forceFullWeeks: !prevState.forceFullWeeks
+    }));
+  }
+
+  toggleShowTodayBtn() {
+    this.setState(prevState => ({
+      showTodayBtn: !prevState.showTodayBtn
+    }));
+  }
+
+  toggleShowWeekSeparators() {
+    this.setState(prevState => ({
+      showWeekSeparators: !prevState.showWeekSeparators
+    }));
+  }
+
+  toggleSelectRange() {
+    this.setState(prevState => ({
+      selectRange: !prevState.selectRange
+    }));
+  }
+
+  selectFirstDayOfWeek(event) {
+    this.setState({
+      firstDayOfWeek: parseInt(event.target.value, 10)
+    });
+  }
+
+  render() {
+    const {
+      year,
+      showTodayBtn,
+      selectedDay,
+      showDaysOfWeek,
+      forceFullWeeks,
+      showWeekSeparators,
+      firstDayOfWeek,
+      selectRange,
+      selectedRange,
+      customCSSclasses
+    } = this.state;
+
+    return (
+      <div>
+        <div id="calendar">
+          <CalendarControls
+            year={year}
+            showTodayButton={showTodayBtn}
+            onPrevYear={() => this.onPrevYear()}
+            onNextYear={() => this.onNextYear()}
+            goToToday={() => this.goToToday()}
+          />
+          <Calendar
+            year={year}
+            selectedDay={selectedDay}
+            showDaysOfWeek={showDaysOfWeek}
+            forceFullWeeks={forceFullWeeks}
+            showWeekSeparators={showWeekSeparators}
+            firstDayOfWeek={firstDayOfWeek}
+            selectRange={selectRange}
+            selectedRange={selectedRange}
+            onPickDate={(date, classes) => this.datePicked(date, classes)}
+            onPickRange={(start, end) => this.rangePicked(start, end)}
+            customClasses={customCSSclasses}
+          />
+        </div>
+
+      
+
+        <div className="options">
+          <b>Demo Options</b>
+          <br />
+          <ul>
+            <li>
+              <label htmlFor="firstDayOfWeek">First day of week</label>
+              <select id="firstDayOfWeek" value={firstDayOfWeek} onChange={e => this.selectFirstDayOfWeek(e)}>
+                {[0, 1, 2, 3, 4, 5, 6].map(i => (
+                  <option key={i} value={i}>
+                    {moment()
+                      .weekday(i)
+                      .format('ddd')}
+                  </option>
+                ))}
+              </select>
+            </li>
+            <li>
+              <input id="selectRange" type="checkbox" checked={selectRange} onChange={() => this.toggleSelectRange()} />
+              <label htmlFor="selectRange">Select Date range</label>
+            </li>
+          </ul>
+         
+        
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/Calendar.js
@@ -0,0 +1,170 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import moment from 'moment';
+import { momentObj } from 'react-moment-proptypes';
+import Month from './Month';
+
+
+export function range(start, stop, step) {
+  if (stop == null) {
+    stop = start || 0;
+    start = 0;
+  }
+  step = step || 1;
+
+  const length = Math.max(Math.ceil((stop - start) / step), 0);
+  const range = Array(length);
+
+  for (let idx = 0; idx < length; idx++, start += step) {
+    range[idx] = start;
+  }
+
+  return range;
+}
+
+const propTypes = {
+  year: PropTypes.number.isRequired,
+  forceFullWeeks: PropTypes.bool,
+  showDaysOfWeek: PropTypes.bool,
+  showWeekSeparators: PropTypes.bool,
+  firstDayOfWeek: PropTypes.number,
+  useIsoWeekday: PropTypes.bool,
+  selectRange: PropTypes.bool,
+  onPickDate: PropTypes.func,
+  onPickRange: PropTypes.func,
+  selectedDay: momentObj,
+  customClasses: PropTypes.oneOfType([PropTypes.object, PropTypes.func]),
+  titles: PropTypes.func
+};
+
+const defaultProps = {
+  forceFullWeeks: false,
+  showDaysOfWeek: true,
+  showWeekSeparators: true,
+  firstDayOfWeek: 0,
+  useIsoWeekday: false,
+  selectRange: false,
+  onPickDate: null,
+  onPickRange: null,
+  selectedDay: moment(),
+  customClasses: null,
+  titles: null
+};
+
+class Calendar extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      selectingRange: undefined
+    };
+  }
+
+  dayClicked(date, classes) {
+    if (!date) {
+      // clicked on prev or next month
+      return;
+    }
+
+    let { selectingRange } = this.state;
+    const { selectRange, onPickRange, onPickDate } = this.props;
+
+    if (!selectRange) {
+      if (onPickDate instanceof Function) {
+        onPickDate(date, classes);
+      }
+      return;
+    }
+
+    if (!selectingRange) {
+      selectingRange = [date, date];
+    } else {
+      if (onPickRange instanceof Function) {
+        if (selectingRange[0] > date) {
+          onPickRange(date, selectingRange[0]);
+        } else {
+          onPickRange(selectingRange[0], date);
+        }
+      }
+      selectingRange = undefined;
+    }
+
+    this.setState({
+      selectingRange
+    });
+  }
+
+  dayHovered(hoveredDay) {
+    if (!hoveredDay) {
+      // clicked on prev or next month
+      return;
+    }
+
+    const { selectingRange } = this.state;
+
+    if (selectingRange) {
+      selectingRange[1] = hoveredDay;
+
+      this.setState({
+        selectingRange
+      });
+    }
+  }
+
+  renderDaysOfWeek() {
+    const { useIsoWeekday, firstDayOfWeek, forceFullWeeks, showWeekSeparators } = this.props;
+    const totalDays = forceFullWeeks ? 42 : 37;
+
+    const days = [];
+    range(firstDayOfWeek, totalDays + firstDayOfWeek).forEach(i => {
+      const momentDay = useIsoWeekday ? moment().isoWeekday(i) : moment().weekday(i);
+      const day = momentDay.format('ddd').charAt(0);
+
+      if (showWeekSeparators) {
+        if (i % 7 === firstDayOfWeek && days.length) {
+          // push week separator
+          days.push(<th className="week-separator" key={`seperator-${i}`} />);
+        }
+      }
+      days.push(
+        <th key={`weekday-${i}`} className={i % 7 === 0 ? 'bolder' : ''}>
+          {day}
+        </th>
+      );
+    });
+
+    return (
+      <tr>
+        <th>&nbsp;</th>
+        {days}
+      </tr>
+    );
+  }
+
+  render() {
+    const { selectingRange } = this.state;
+
+    const months = range(3, 10).map(month => (
+      <Month
+        month={month}
+        key={`month-${month}`}
+        dayClicked={(d, classes) => this.dayClicked(d, classes)}
+        dayHovered={d => this.dayHovered(d)}
+        {...this.props}
+        selectingRange={selectingRange}
+      />
+    ));
+
+    return (
+      <table className="calendar">
+        <thead className="day-headers">{this.props.showDaysOfWeek ? this.renderDaysOfWeek() : null}</thead>
+        <tbody>{months}</tbody>
+      </table>
+    );
+  }
+}
+
+Calendar.propTypes = propTypes;
+Calendar.defaultProps = defaultProps;
+
+export default Calendar;
